ec75bd411736cfebd43db272a010ad5358dd74f5
tbzmike <40115947+tbzmike@users.noreply.github.com>
2026-09-04 17:05:29 +0200

    Bump True RAM Usage to 0.3.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.tbzmike.trueramusage"
         minSdk = 26
         targetSdk = 37
-        versionCode = 5
-        versionName = "0.3.2"
+        versionCode = 6
+        versionName = "0.3.3"
     }
 
     signingConfigs {
