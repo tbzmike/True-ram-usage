563e8715631bdee23a74699bf4abdff68c3657ea
tbzmike <40115947+tbzmike@users.noreply.github.com>
2026-09-04 15:50:53 +0200

    Bump True RAM Usage to 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.tbzmike.trueramusage"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
     }
 
     buildFeatures {
