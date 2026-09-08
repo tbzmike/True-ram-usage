b3571652719722ae51787e56da2f00cee128c3be
tbzmike <40115947+tbzmike@users.noreply.github.com>
2026-09-08 14:29:37 +0200

    Bump version for aggressive reclaim

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.tbzmike.trueramusage"
         minSdk = 26
         targetSdk = 37
-        versionCode = 11
-        versionName = "0.6.0"
+        versionCode = 12
+        versionName = "0.7.0"
     }
 
     signingConfigs {
